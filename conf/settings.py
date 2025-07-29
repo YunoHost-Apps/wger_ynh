4b1d84903673e6421837953cb25c29a1453fcfbe
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-07-29 08:44:22 +0200

    test

--- conf/settings.py
+++ conf/settings.py
@@ -1,3 +1,17 @@
+################################################################################
+################################################################################
+##                             FOR YUNOHOST USERS                             ##
+################################################################################
+################################################################################
+
+# Please do not modify this file, it will be reset at the next update.
+# You can edit the file __INSTALL_DIR__/local_settings.py and add/modify the settings you need.
+# The parameters you add in local_settings.py will overwrite these,
+# but you can use the options and documentation in this file to find out what can be done.
+
+################################################################################
+################################################################################
+
 #!/usr/bin/env python
 
 # Third Party
@@ -21,12 +35,12 @@ if os.environ.get('DJANGO_ADMINS'):
 if os.environ.get("DJANGO_DB_ENGINE"):
     DATABASES = {
         'default': {
-            'ENGINE': env.str("DJANGO_DB_ENGINE"),
-            'NAME': env.str("DJANGO_DB_DATABASE"),
-            'USER': env.str("DJANGO_DB_USER"),
-            'PASSWORD': env.str("DJANGO_DB_PASSWORD"),
-            'HOST': env.str("DJANGO_DB_HOST"),
-            'PORT': env.int("DJANGO_DB_PORT"),
+            'ENGINE': env.str("django.db.backends.postgresql"),
+            'NAME': env.str("__DB_NAME__"),
+            'USER': env.str("__DB_USER__"),
+            'PASSWORD': env.str("__DB_PWD__"),
+            'HOST': env.str("localhost"),
+            'PORT': env.int("5432"),
         }
     }
 else:
@@ -41,7 +55,7 @@ else:
 TIME_ZONE = env.str("TIME_ZONE", 'Europe/Berlin')
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = env.str("SECRET_KEY", 'wger-docker-supersecret-key-1234567890!@#$%^&*(-_)')
+SECRET_KEY = env.str("__KEY__")
 
 # Your reCaptcha keys
 RECAPTCHA_PUBLIC_KEY = env.str('RECAPTCHA_PUBLIC_KEY', '')
@@ -50,12 +64,12 @@ RECAPTCHA_PRIVATE_KEY = env.str('RECAPTCHA_PRIVATE_KEY', '')
 # The site's URL (e.g. http://www.my-local-gym.com or http://localhost:8000)
 # This is needed for uploaded files and images (exercise images, etc.) to be
 # properly served.
-SITE_URL = env.str('SITE_URL', 'http://localhost:8000')
+SITE_URL = env.str('https://__DOMAIN__')
 
 # Path to uploaded files
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = env.str("DJANGO_MEDIA_ROOT", '/home/wger/media')
-STATIC_ROOT = env.str("DJANGO_STATIC_ROOT", '/home/wger/static')
+MEDIA_ROOT = env.str("__DATA_DIR__/media")
+STATIC_ROOT = env.str("__INSTALL_DIR__/static")
 
 # If you change these, adjust nginx alias definitions as well
 MEDIA_URL = env.str('MEDIA_URL', '/media/')
@@ -64,25 +78,25 @@ STATIC_URL = env.str('STATIC_URL', '/static/')
 LOGIN_REDIRECT_URL = env.str('LOGIN_REDIRECT_URL', '/')
 
 # Allow all hosts to access the application. Change if used in production.
-ALLOWED_HOSTS = ['*', ]
+ALLOWED_HOSTS = ['__DOMAIN__']
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
 # Configure a real backend in production
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-if env.bool("ENABLE_EMAIL", False):
+if env.bool("ENABLE_EMAIL", True):
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = env.str("EMAIL_HOST")
-    EMAIL_PORT = env.int("EMAIL_PORT")
-    EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+    EMAIL_HOST = env.str("__DOMAIN__")
+    EMAIL_PORT = env.int("587")
+    EMAIL_HOST_USER = env.str("__APP__")
+    EMAIL_HOST_PASSWORD = env.str("__MAIL_PWD__")
     EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", True)
     EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", False)
     EMAIL_TIMEOUT = 60
 
 # Sender address used for sent emails
-DEFAULT_FROM_EMAIL = env.str("FROM_EMAIL", "wger Workout Manager <wger@example.com>")
+DEFAULT_FROM_EMAIL = env.str("FROM_EMAIL", "wger Workout Manager <__APP__@__DOMAIN__")
 WGER_SETTINGS['EMAIL_FROM'] = DEFAULT_FROM_EMAIL
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 EMAIL_FROM_ADDRESS = DEFAULT_FROM_EMAIL
@@ -94,13 +108,13 @@ WGER_SETTINGS["ALLOW_UPLOAD_VIDEOS"] = env.bool("ALLOW_UPLOAD_VIDEOS", True)
 WGER_SETTINGS["DOWNLOAD_INGREDIENTS_FROM"] = env.str("DOWNLOAD_INGREDIENTS_FROM", "WGER")
 WGER_SETTINGS["EXERCISE_CACHE_TTL"] = env.int("EXERCISE_CACHE_TTL", 3600)
 WGER_SETTINGS["MIN_ACCOUNT_AGE_TO_TRUST"] = env.int("MIN_ACCOUNT_AGE_TO_TRUST", 21)  # in days
-WGER_SETTINGS["SYNC_EXERCISES_CELERY"] = env.bool("SYNC_EXERCISES_CELERY", False)
-WGER_SETTINGS["SYNC_EXERCISE_IMAGES_CELERY"] = env.bool("SYNC_EXERCISE_IMAGES_CELERY", False)
-WGER_SETTINGS["SYNC_EXERCISE_VIDEOS_CELERY"] = env.bool("SYNC_EXERCISE_VIDEOS_CELERY", False)
-WGER_SETTINGS["SYNC_INGREDIENTS_CELERY"] = env.bool("SYNC_INGREDIENTS_CELERY", False)
-WGER_SETTINGS["SYNC_OFF_DAILY_DELTA_CELERY"] = env.bool("SYNC_OFF_DAILY_DELTA_CELERY", False)
+WGER_SETTINGS["SYNC_EXERCISES_CELERY"] = env.bool("SYNC_EXERCISES_CELERY", True)
+WGER_SETTINGS["SYNC_EXERCISE_IMAGES_CELERY"] = env.bool("SYNC_EXERCISE_IMAGES_CELERY", True)
+WGER_SETTINGS["SYNC_EXERCISE_VIDEOS_CELERY"] = env.bool("SYNC_EXERCISE_VIDEOS_CELERY", True)
+WGER_SETTINGS["SYNC_INGREDIENTS_CELERY"] = env.bool("SYNC_INGREDIENTS_CELERY", True)
+WGER_SETTINGS["SYNC_OFF_DAILY_DELTA_CELERY"] = env.bool("SYNC_OFF_DAILY_DELTA_CELERY", True)
 WGER_SETTINGS["USE_RECAPTCHA"] = env.bool("USE_RECAPTCHA", False)
-WGER_SETTINGS["USE_CELERY"] = env.bool("USE_CELERY", False)
+WGER_SETTINGS["USE_CELERY"] = env.bool("USE_CELERY", True)
 
 #
 # Auth Proxy Authentication
@@ -189,8 +203,8 @@ REST_FRAMEWORK['NUM_PROXIES'] = env.int('NUMBER_OF_PROXIES', 1)
 #
 # Celery message queue configuration
 #
-CELERY_BROKER_URL = env.str("CELERY_BROKER", "redis://cache:6379/2")
-CELERY_RESULT_BACKEND = env.str("CELERY_BACKEND", "redis://cache:6379/2")
+CELERY_BROKER_URL = env.str("CELERY_BROKER", "redis://127.0.0.1:6379/__REDIS_DB__")
+CELERY_RESULT_BACKEND = env.str("CELERY_BACKEND", "redis://127.0.0.1:6379/__REDIS_DB__")
 
 #
 # Prometheus metrics
